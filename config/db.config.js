04a4e8a1ef6deb498fa9b692371fa72260271ccd
celso <celso.san2101@alumnos.ubiobio.cl>
2025-07-20 00:44:45 -0400

    cambio

--- config/db.config.js
+++ config/db.config.js
@@ -29,6 +29,11 @@ const dbConfig = {
     dialectOptions: {} // vacío porque no usas SSL
 };
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
+
 module.exports = dbConfig;
 
 
